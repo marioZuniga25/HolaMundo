f23e470d8b2ec64b702c4756cf8bd703b9c21e60
marioZuniga25 <mariogt367@gmail.com>
2023-02-23 20:43:02 -0600

    Esta es mi primer versión

---
+++ src/hello/Hello.java
@@ -0,0 +1,19 @@
+
+package hello;
+
+/**
+ *
+ * @author Mayo
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo");
+        
+    }
+    
+}
